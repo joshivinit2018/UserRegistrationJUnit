11268eedcaebf9e7bcdbbf098e770e1629ea992e
joshivinit2018 <joshivinit2018@gmailcom>
2021-04-25 10:29:45 +0530

    [vinit]:add welcome

---
+++ src/test/java/com/bridgelabz/UserValidatorTest.java
@@ -0,0 +1,19 @@
+package com.bridgelabz;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class UserValidatorTest {
+    @Test
+    void givenFirstName_WhenProper_ShouldReturnTrue() {
+        UserValidator validator = new UserValidator();
+        boolean result = UserValidator.validateFirstName("Vinit");
+        Assertions.assertEquals(true, result);
+    }
+    @Test
+    public void givenFirstName_WhenShort_ShouldReturnFalse() {
+        UserValidator validator = new UserValidator();
+        boolean result = UserValidator.validateFirstName("Vi");
+        Assertions.assertEquals(false,result);
+    }
+}
