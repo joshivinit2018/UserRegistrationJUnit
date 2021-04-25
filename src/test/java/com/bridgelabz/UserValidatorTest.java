cfbe9f3afaf6761c97e51e223bc62661c4968db3
joshivinit2018 <joshivinit2018@gmailcom>
2021-04-25 10:40:44 +0530

    [vinit]:add As user need enter a valid last name

--- src/test/java/com/bridgelabz/UserValidatorTest.java
+++ src/test/java/com/bridgelabz/UserValidatorTest.java
@@ -16,4 +16,18 @@ public class UserValidatorTest {
         boolean result = UserValidator.validateFirstName("Vi");
         Assertions.assertEquals(false, result);
     }
+
+    @Test
+    public void givenLastName_WhenProper_ShouldReturnTrue() {
+        UserValidator validator = new UserValidator();
+        boolean result = UserValidator.validateLastName("Joshi");
+        Assertions.assertEquals(true,result);
+    }
+
+    @Test
+    public void givenLastName_WhenShort_ShouldReturnFalse() {
+        UserValidator validator = new UserValidator();
+        boolean result = UserValidator.validateLastName("jo");
+        Assertions.assertEquals(false,result);
+    }
 }
